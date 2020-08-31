7d053b042e999b891212ee9848394aa740b2d97a
KimHor-Bour <bourkimhor@gmail.com>
2020-08-31 18:22:28 +0700

    Botton Navigation

--- Desktop/KESSSeller-master/app/src/main/java/com/example/kessseller/XML/BookingScreen.java
+++ Desktop/KESSSeller-master/app/src/main/java/com/example/kessseller/XML/BookingScreen.java
@@ -1,11 +1,16 @@
 package com.example.kessseller.XML;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.kessseller.Java.Viewpager;
@@ -21,7 +26,6 @@ public class BookingScreen extends AppCompatActivity {
     private StatusMyorderBooking tab3;
 
     LinearLayout filter;
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
